54f5566769c5992c5ee231f823343b075d10d6e6
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-01-09 11:41:39 -0300

    Íconos agregados al archivo JavaScript!

---
+++ ArchivosEjecutables/script.js
@@ -0,0 +1,22 @@
+let icons = [];
+let select = [];
+
+generarTablero();
+
+function loadIcons()
+{
+    icons = [
+        '<i class="fa-regular fa-user"></i>',
+        '<i class="fa-solid fa-user"></i>',
+        '<i class="fa-solid fa-star"></i>',
+        '<i class="fa-regular fa-star"></i>',
+        '<i class="fa-solid fa-eye"></i>',
+        '<i class="fa-solid fa-truck"></i>',
+        '<i class="fa-solid fa-droplet"></i>',
+        '<i class="fa-regular fa-circle-exclamation"></i>',
+        '<i class="fa-regular fa-fire"></i>',
+        '<i class="fa-solid fa-dice"></i>',
+        '<i class="fa-regular fa-triangle-exclamation"></i>',
+        '<i class="fa-solid fa-star-and-crescent"></i>',
+    ]
+}
